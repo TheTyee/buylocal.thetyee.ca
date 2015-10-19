355dd623cf994a899cb0680fd56c195d4727b340
Phillip Smith <ps@phillipadsmith.com>
2015-10-19 14:06:53 -0700

    Commit static app template

---
+++ ui/js/app.js
@@ -0,0 +1,7 @@
+/* App JS */
+// Libraries concatenated & compressed by by jekyll-assets pipeline
+//= require jquery.js
+//= require modernizr.js
+//= require bootstrap.js
+//= require underscore.js
+//= require backbone.js
