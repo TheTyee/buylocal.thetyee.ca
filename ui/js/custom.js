b29f955e90cf3c417e36fb6d4d37ec38420b59ad
Sally Poulsen <sally@Sallys-MacBook-Pro.local>
2015-11-10 16:54:52 -0700

    Adds randomizer to the partner ads.

--- ui/js/custom.js
+++ ui/js/custom.js
@@ -1,29 +1,46 @@
 $(document).ready(function(){
   	var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1bpwVmVv4J7ZpXi0BZeG8sH93jhHFD6CCTxtzmin3SeA/pubhtml';
-
 	  function init() {
 	    var tabletop = Tabletop.init( { key: public_spreadsheet_url, callback: showInfo } )
 	  }
 
 	  function showInfo(data, tabletop) {
 	  	
-	  		all_partners = data.Sheet1.elements;
-		console.log(all_partners);
+	  	all_partners = data.Sheet1.elements;
+		total_partners = all_partners.length;
 
+		randomizer = Math.floor((Math.random() * 10) + 1);
+
+
+
+			var template;
+			var each_rendered;
+			var view;
+	
+			//Math.floor((Math.random() * 10) + 1);
+			view = all_partners[randomizer];
+			template = $('#partner_template').html();
+			//Mustache.parse(template);   // optional, speeds up future uses
+			each_rendered = Mustache.render(template, view);
+			console.log(template);
+			$('#partners').html(each_rendered);
+
+/*
 		$.each(all_partners, function(index, value){
-			console.log(value);
+			//console.log(value);
 	
 			var template;
 			var each_rendered;
 			var view;
-
+	
+			//Math.floor((Math.random() * 10) + 1);
 			view = value;
 			template = $('#partner_template').html();
 			//Mustache.parse(template);   // optional, speeds up future uses
 			each_rendered = Mustache.render(template, view);
 			console.log(template);
-			$('#partners').append(each_rendered);
-			});
+			$('#partners').html(each_rendered);
+			});*/
 		}
 
 init();
