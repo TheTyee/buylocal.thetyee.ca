980bbcada575fbafb8f13965e766461c2feed5df
Sally Poulsen <sally@Sallys-MacBook-Pro.local>
2015-11-11 19:29:54 -0700

    Syntax for Twitter sharing on cards.

--- ui/js/app.js
+++ ui/js/app.js
@@ -34,7 +34,7 @@ App.CardView = Backbone.View.extend({
     initialize: function () {
 
     },
-    template: _.template("<li data-card='<%= EntryId %>' class='card'><%= DateCreated %><br /><a href='/#show/<%= EntryId %>'>Social Sharing</a><br /><a href='/letters/#show/<%= EntryId %>'>Permalink</a><br />Dear <%= Field652 %><br /><%= Field655 %></li>"),
+    template: _.template("<li data-card='<%= EntryId %>' class='card'><%= DateCreated %><br /><a href='https://twitter.com/share?url=http://localhost:4000/letters/show/<%= EntryId %>&text=Thank you, <%= Field652 %>!&via=TheTyee&hashtags=bcbuylocal' target='_blank'>Twitter</a><br /><a href='/letters/#show/<%= EntryId %>'>Permalink</a><br />Dear <%= Field652 %><br /><%= Field655 %></li>"),
     render: function() {
         this.$el.html(this.template(this.model.attributes));
         return this;
@@ -50,7 +50,7 @@ App.CardDetailView = Backbone.View.extend({
     initialize: function () {
 
     },
-    template: _.template("<div data-card='<%= EntryId %>' class='card-detail'><%= DateCreated %><br /><a href='/#show/<%= EntryId %>'>Social Sharing</a><br /><a href='/letters/#show/<%= EntryId %>'>Permalink</a><br />Dear <%= Field652 %>:<br /> <%= Field655 %></div><br /><a class='show-list' href='#'>Back</a>"),
+    template: _.template("<div data-card='<%= EntryId %>' class='card-detail'><%= DateCreated %><br /><a href='https://twitter.com/share?url=http://localhost:4000/letters/show/<%= EntryId %>&text=Thank you, <%= Field652 %>!&via=TheTyee&hashtags=bcbuylocal ' target='_blank'>Twitter</a><br /><a href='/letters/#show/<%= EntryId %>'>Permalink</a><br />Dear <%= Field652 %>:<br /> <%= Field655 %></div><br /><a class='show-list' href='#'>Back</a>"),
     render: function() {
         this.$el.show();
         this.$el.html(this.template(this.model.attributes));
