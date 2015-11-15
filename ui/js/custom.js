1186a3ff688f8ecce9dbf1c4a997aa02069d0942
Phillip Smith <ps@phillipadsmith.com>
2015-11-14 22:43:21 -0800

    Remove promo handling from custom.js

--- ui/js/custom.js
+++ ui/js/custom.js
@@ -1,102 +1,56 @@
 $(document).ready(function(){
 
-	//generate letters on the home page, without any backbone bells and whistles
-	
-	$.getJSON( App.apiUrl + '/api/v1/letters', function(data) {
-  	
+    //generate letters on the home page, without any backbone bells and whistles
 
-  		for (i=0;i<3;++i){	
-			indexLetters = data.data.letters[i];
-			console.log(indexLetters);
-			html = "<article data-card='"+ indexLetters.entry_id +"' class='card'>";
-			html += "<header><p class='dateCreated'>"+indexLetters.date_created+"</p>";
-			html +="<a href='https://twitter.com/share?url=http://"+window.apiUrl+"/letters/show/<%= id %>&text=Thank you, "+ indexLetters.business_name+"!&via=TheTyee&hashtags=bcbuylocal ' target='_blank'><i class='fa fa-twitter-square fa-2x'></i></a>";
-	    	html += "<a href='https://www.facebook.com/dialog/send?app_id=441246329398694&name=A Buy Local Thank You to "+ indexLetters.business_name+"&description=test&link=http://"+window.apiUrl+"/letters/show/"+ indexLetters.entry_id +"&redirect_uri=http://buylocal.thetyee.ca/fbr.php' target='_blank'><i class='fa fa-facebook-square fa-2x'></i></a>";
-    		html += "<a class='email-share' target='_blank' href='mailto:?subject=Buy Local&body=I thought you might like to see this! Read it at http://"+window.apiUrl+"/letters/show/"+ indexLetters.entry_id +"'><i class='fa fa-envelope fa-2x'></i></a></header>";
-    		html += "<p class='businessName'>Dear "+ indexLetters.business_name+",</p>";
-    		html += "<p class='loveMessage'>"+indexLetters.letter_text+"</p>";
-    		html += "<p class='submitterName'>- "+indexLetters.first_name+"</p>";
-		    html += "<footer><p class='permalink'><small><a href='/letters/#show/"+indexLetters.entry_id+"'>Permalink</a></small></p>";
-		    html += "</footer></article>";
-
-			$('.index-letter-'+i).html(html);
-
-		}
+    $.getJSON( App.apiUrl + '/api/v1/letters', function(data) {
 
 
+        for (i=0;i<3;++i){	
+            indexLetters = data.data.letters[i];
+            //console.log(indexLetters);
+            html = "<article data-card='"+ indexLetters.entry_id +"' class='card'>";
+            html += "<header><p class='dateCreated'>"+indexLetters.date_created+"</p>";
+            html +="<a href='https://twitter.com/share?url=http://"+window.apiUrl+"/letters/show/<%= id %>&text=Thank you, "+ indexLetters.business_name+"!&via=TheTyee&hashtags=bcbuylocal ' target='_blank'><i class='fa fa-twitter-square fa-2x'></i></a>";
+            html += "<a href='https://www.facebook.com/dialog/send?app_id=441246329398694&name=A Buy Local Thank You to "+ indexLetters.business_name+"&description=test&link=http://"+window.apiUrl+"/letters/show/"+ indexLetters.entry_id +"&redirect_uri=http://buylocal.thetyee.ca/fbr.php' target='_blank'><i class='fa fa-facebook-square fa-2x'></i></a>";
+            html += "<a class='email-share' target='_blank' href='mailto:?subject=Buy Local&body=I thought you might like to see this! Read it at http://"+window.apiUrl+"/letters/show/"+ indexLetters.entry_id +"'><i class='fa fa-envelope fa-2x'></i></a></header>";
+            html += "<p class='businessName'>Dear "+ indexLetters.business_name+",</p>";
+            html += "<p class='loveMessage'>"+indexLetters.letter_text+"</p>";
+            html += "<p class='submitterName'>- "+indexLetters.first_name+"</p>";
+            html += "<footer><p class='permalink'><small><a href='/letters/#show/"+indexLetters.entry_id+"'>Permalink</a></small></p>";
+            html += "</footer></article>";
 
-  	//console.log( data );
-});
+            $('.index-letter-'+i).html(html);
 
+        }
 
-	//Initialize partner ads through tabletop
-  	
-  	var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1bpwVmVv4J7ZpXi0BZeG8sH93jhHFD6CCTxtzmin3SeA/pubhtml';
-	function init() {
-		var tabletop = Tabletop.init( { key: public_spreadsheet_url, callback: showInfo } )
-	}
+    });
 
-	function showInfo(data, tabletop) {
-		all_partners = data.Sheet1.elements;
-		total_partners = all_partners.length;
-	//Create an array of 3 unmatching numbers that i can retrieve from the spreadsheet onpage load
+    //Initialize footer content through tabletop
+    var footer_content = 'https://docs.google.com/spreadsheets/d/1OHdC2tYZz--Q3ksP2qk8AzBd8jSNp8qSElsBB3sCsGY/pubhtml';
 
-		for (var a=[],i=0;i<total_partners;++i) a[i]=i;	
-			function shuffle(array) {
-			  var tmp, current, top = array.length;
-			  if(top) while(--top) {
-			    current = Math.floor(Math.random() * (top + 1));
-			    tmp = array[current];
-			    array[current] = array[top];
-			    array[top] = tmp;
-			  }
-			  return array;
-		}
-		a = shuffle(a);
-		//limit it to 3, since that's all we'll show
-		randomizer = a.slice(0,3);
+    function footer_init() {
+        var tabletop = Tabletop.init({ 
+            key: footer_content, 
+            callback: footer_showInfo 
+        });
+    }
 
-		$.each(randomizer, function(key, value){
-			var template;
-			var each_rendered;
-			var view;
-			view = all_partners[value];
-			template = $('#partner_template').html();
-			each_rendered = Mustache.render(template, view);
-				$('.partner-'+key).html(each_rendered);
-			});
-		}
+    function footer_showInfo(data, tabletop) {
 
-	init();
+        all_partners = data.Sheet1.elements;
 
-	//Initialize footer content through tabletop
+        $.each(all_partners, function(key, value){
+            var template;
+            var each_rendered;
+            var view;
+            view = value;
+            template = $('#footer_template').html();
+            each_rendered = Mustache.render(template, view);
+            //console.log(template);
+            $('#footer').append(each_rendered);
+        });
+    }
 
-	var footer_content = 'https://docs.google.com/spreadsheets/d/1OHdC2tYZz--Q3ksP2qk8AzBd8jSNp8qSElsBB3sCsGY/pubhtml';
+    footer_init();
 
-	 function footer_init() {
-	    var tabletop = Tabletop.init( { key: footer_content, callback: footer_showInfo } )
-	  }
-
-	  function footer_showInfo(data, tabletop) {
-	  	
-	  		all_partners = data.Sheet1.elements;
-
-	  		$.each(all_partners, function(key, value){
-				var template;
-				var each_rendered;
-				var view;
-				view = value;
-				template = $('#footer_template').html();
-				each_rendered = Mustache.render(template, view);
-				//console.log(template);
-				$('#footer').append(each_rendered);
-			});
-	}
-
-	footer_init();
-
-
-	//$(".letters > div:nth-child(9)").css('background', 'green');
-	//$('.dateCreated').css('background', 'blue');
-	
 });
